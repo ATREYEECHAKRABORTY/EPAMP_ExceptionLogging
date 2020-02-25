680283288ac6299538448238adb145e60ff2655c
Atsy21 <atreyee.1999.ac@gmail.com>
2020-02-25 20:15:46 +0530

    This is EPAM's Task on Logging

---
+++ Exception_Logger/src/test/java/com/EPAM_week5/Exception_Logger/AppTest.java
@@ -0,0 +1,20 @@
+package com.EPAM_week5.Exception_Logger;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
